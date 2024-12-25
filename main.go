f34c41fcb0aa42e77a2cc37d2c518dd261f37a71
ZeraiGR <kirillgr15@mail.ru>
2024-12-25 19:49:07 +0300

    fifth iteration

--- main.go
+++ main.go
@@ -1,103 +1,111 @@
 package main
 
 import (
- "fmt"
- "io"
- "net/http"
- "strconv"
- "strings"
- "time"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 const (
- url           = "http://srv.msk01.gigacorp.local/_stats"
- maxAttempts   = 3
- sleepTime     = time.Second
- memoryLimit   = 80
- diskLimit     = 90
- bandwidthLimit = 90
- loadLimit     = 30
+	url           = "http://srv.msk01.gigacorp.local/_stats"
+	maxRetries   = 3
+	sleepTime     = time.Second
+	memoryLimit   = 80
+	diskLimit     = 90
+	bandwidthLimit = 90
+	loadLimit     = 30
 )
 
-var failedAttempts int
+var currentRetries int
 
 func main() {
- for {
-  if !fetchAndProcessStats() {
-   handleFailedAttempt()
-  }
-  time.Sleep(sleepTime)
- }
-}
-
-func fetchAndProcessStats() bool {
- response, err := http.Get(url)
- if err != nil || response.StatusCode != http.StatusOK {
-  return false
- }
- defer response.Body.Close()
-
- data, err := parseResponseBody(response.Body)
- if err != nil {
-  return false
- }
-
- analyzeData(data)
- return true
+	fetchStatistics()
 }
 
 func parseResponseBody(body io.Reader) ([7]int, error) {
- var data [7]int
+	var data [7]int{}
 
- rawBody, err := io.ReadAll(body)
- if err != nil {
-  return data, err
- }
+	rawBody, err := io.ReadAll(body)
 
- rawDataArray := strings.Split(strings.TrimSpace(string(rawBody)), ",")
- if len(rawDataArray) != 7 {
-  return data, fmt.Errorf("unexpected data length")
- }
+	if err != nil {
+		return fetchWithMaxRetry()
+	}
 
- for index, element := range rawDataArray {
-  i, err := strconv.Atoi(strings.TrimSpace(element))
-  if err != nil {
-   return data, err
-  }
-  data[index] = i
- }
- return data, nil
+	rawDataArray := strings.Split(string(body), ",")
+	
+	if len(rawDataArray) != 7 {
+		return fetchWithMaxRetry()
+	}
+
+	var isValidData = true
+	for index, element := range rawDataArray {
+		i, err := strconv.Atoi(element)
+
+		if err != nil {
+			isValidData = false
+			break;
+		}
+
+		data[index] = i
+	}
+
+	if !isValidData {
+		return fetchWithMaxRetry()
+	}
+
+	return data, nil
 }
 
 func analyzeData(data [7]int) {
- // Check Load Average
- if data[0] > loadLimit {
-  fmt.Printf("Load Average is too high: %d\n", data[0])
- }
+	if data[0] > 30 {
+		fmt.Printf("Load Average is too high: %d\n", data[0])
+	}
 
- // Check Memory usage
- memoryUsage := data[2] * 100 / data[1]
- if memoryUsage > memoryLimit {
-  fmt.Printf("Memory usage too high: %d%%\n", memoryUsage)
- }
+	memoryUsage := data[2] * 100 / data[1]
+	if memoryUsage > 80 {
+		fmt.Printf("Memory usage too high: %d%%\n", memoryUsage)
+	}
 
- // Check free disk space
- freeDiskSpace := (data[3] - data[4]) / (1024 * 1024)
- if (data[3]-data[4])*100/data[3] < (100 - diskLimit) {
-  fmt.Printf("Free disk space is too low: %d Mb left\n", freeDiskSpace)
- }
+	if data[4] * 100 / data[3] > 90 {
+		freeDiskSpace := (data[3] - data[4]) / (1024*1024)
+		fmt.Printf("Free disk space is too low: %d Mb left\n", freeDiskSpace)
+	}
 
- // Check network bandwidth usage
- freeBandwidth := (data[5] - data[6]) * 8 / 1000000 // Convert bytes to megabits
- if (data[5]-data[6])*100/data[5] < (100 - bandwidthLimit) {
-  fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeBandwidth)
- }
+	if data[6] * 100 / data[5] > 90 {
+		freeBandwidth := (data[5] - data[6]) / 1000000
+		fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeBandwidth)
+	}
 }
 
-func handleFailedAttempt() {
- failedAttempts++
- if failedAttempts >= maxAttempts {
-  fmt.Println("Unable to fetch server statistic")
-  failedAttempts = 0
- }
+func fetchStatistics() {
+	response, err := http.Get(url)
+
+	if err != nil || response.StatusCode != http.StatusOK {
+		return fetchWithMaxRetry()
+	}
+
+	data, err := parseResponseBody(response.Body)
+
+	if err != nil {
+		return fetchWithMaxRetry()
+	}
+
+	analyzeData(data)
+
+	defer response.Body.Close()
+}
+
+func fetchWithMaxRetry() {
+	currentRetries++
+
+	if currentRetries >= maxRetries {
+		fmt.Println("Unable to fetch server statistic")
+		currentRetries = 0
+	}
+
+	time.Sleep(time.Second)
+	fetchStatistics()
 }
